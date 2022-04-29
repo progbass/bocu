844859517eb6e17ba7a7a984f1d43f28f0fcfab9
Israel Diaz <progbass@gmail.com>
2022-04-29 00:25:40 -0500

    Added .env file and library

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const functions = require("firebase-functions");
 const app = require('express')();
 const cors = require('cors');
--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
-	apiKey: "AIzaSyB8eKKhyVbQOrSybfTLLVrpPWagN_SkFAE",
-	authDomain: "bocu-b909d.firebaseapp.com",
-	projectId: "bocu-b909d",
-	storageBucket: "bocu-b909d.appspot.com",
-	messagingSenderId: "540422971178",
-	appId: "1:540422971178:web:0eb8f1a90c45c4c5fb0853",
-	measurementId: "G-R1GNFP6VW8",
-	databaseURL: "https://bocu-b909d.firebaseio.com"
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
+	measurementId: process.env.MEASUREMENT_ID,
+	databaseURL: process.env.DATABASE_URL
   };
