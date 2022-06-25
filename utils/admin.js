d872d5c861f3ce43dcfb81c6a07a92adc6f77671
Israel Diaz <progbass@gmail.com>
2022-06-25 01:18:01 -0500

    added firestore emulator

--- utils/admin.js
+++ utils/admin.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const app = require('firebase-admin');
 const { applicationDefault } = require("firebase-admin/app");
 const { getDatabase } = require("firebase/database");
+const { connectFirestoreEmulator } = require("firebase/firestore");
 const { getFirestore } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 const config = require('./config');
@@ -20,5 +21,6 @@ app.initializeApp({
 const auth = getAuth(); //app.auth(); //
 //const db = {}//getDatabase();
 const db = getFirestore();
+//connectFirestoreEmulator(db, 'localhost', 8080);
 
 module.exports = { app, db, auth };
