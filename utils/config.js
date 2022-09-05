d1bcf003a27201269cc009882575798e95f8bb68
Israel Diaz <progbass@gmail.com>
2022-09-04 21:53:17 -0500

    updated app config file

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 
-module.exports = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.STORAGE_BUCKET,
-	messagingSenderId: process.env.MESSAGING_SENDER_ID,
-	appId: process.env.APP_ID,
-	measurementId: process.env.MEASUREMENT_ID,
-	databaseURL: process.env.DATABASE_URL,
-	algoliaAppId: process.env.ALGOLIA_APP_ID,
-	algoliaAdminApiKey: process.env.ALOGLIA_ADMIN_API_KEY
-  };
+const configVars = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+  databaseURL: process.env.DATABASE_URL,
+  algoliaAppId: process.env.ALGOLIA_APP_ID,
+  algoliaAdminApiKey: process.env.ALOGLIA_ADMIN_API_KEY,
+};
+exports.configVars = configVars;
+
+exports.firebaseAppConfig = {
+  apiKey: configVars.apiKey,
+  authDomain: configVars.authDomain,
+  projectId: configVars.projectId,
+  storageBucket: configVars.storageBucket,
+  messagingSenderId: configVars.messagingSenderId,
+  appId: configVars.appId,
+  measurementId: configVars.measurementId,
+  databaseURL: configVars.databaseURL
+};
