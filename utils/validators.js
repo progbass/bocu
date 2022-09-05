6b09342bb377baee50f57187fbed1d6f8397afce
Israel Diaz <progbass@gmail.com>
2022-09-04 21:58:03 -0500

    updated login validators

--- utils/validators.js
+++ utils/validators.js
@@ -5,8 +5,8 @@ const isEmpty = (string) => {
 
 exports.validateLoginData = (data) => {
    let errors = {};
-   if (isEmpty(data.email)) errors.email = 'Must not be empty';
-   if (isEmpty(data.password)) errors.password = 'Must not be  empty';
+   if (!data.email || isEmpty(data.email)) errors.email = 'Must not be empty';
+   if (!data.password || isEmpty(data.password)) errors.password = 'Must not be  empty';
    return {
        errors,
        valid: Object.keys(errors).length === 0 ? true : false
